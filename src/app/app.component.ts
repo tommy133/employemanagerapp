fc6fe9bf78ad4f09d6d6d318643648fff6cb4420
Tomeu Estrany <tomestrany@gmail.com>
2022-10-12 07:33:54 +0200

    UI Modal Logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,8 +24,29 @@ export class AppComponent implements OnInit{
         this.employees = response;
       },
       (error: HttpErrorResponse) => {
-        alert(error.message); 
+        alert(error.message);
       }
     );
+  }
+
+  public onOpenModal(employee: Employee, mode: string): void {
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+
+    if (mode == 'add') {
+      button.setAttribute('data-target', '#addEmployeeModal');
+    }
+    if (mode == 'edit') {
+      button.setAttribute('data-target', '#updateEmployeeModal');
+    }
+    if (mode == 'delete') {
+      button.setAttribute('data-target', '#deleteEmployeeModal');
+    }
+
+    container?.appendChild(button);
+    button.click();
   }
 }
